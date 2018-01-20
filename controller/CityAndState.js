4103a5232f197b73f7accfce1e857de1b1b0e1a8
manishdixit18 <manishdixit18@gmail.com>
2018-01-20 17:30:18 +0530

    Wrapper APis source Error handling + city & State master api bug resolved

--- controller/CityAndState.js
+++ controller/CityAndState.js
@@ -5,7 +5,8 @@ CityAndState.getCityAndState = function(req, res, next) {
     var pin=req.body.PinCode;
     query="call usp_load_pincodewise_detail(?)";
     con.execute_proc(query,[pin],function(data){
-        if(data && data[0]){
+        console.log(JSON.stringify(data));
+        if(data.length>0 & data[0].length>0){
             base.send_response("success",data[0][0],res);
         }else{
             base.send_response("Failure",null,res);
--- controller/baseController.js
+++ controller/baseController.js
@@ -1,16 +1,23 @@
 var Cache = require('../model/cache.js');
 class BaseController{};
 BaseController.send_response = function (message,data, res, next) {
-    if (data) {
-        Message = message;
-        Status = "success";
-        StatusNo = 0;
-        MasterData = data;
-    } else {
-        Message = message;
+    if(data.hasOwnProperty('ExceptionMessage')){
+        Message = "Source Error : " +data.ExceptionMessage;
         Status = "failure";
         StatusNo = 1;
-        MasterData = data;
+        MasterData = null;
+    }else{    
+        if (data) {
+            Message = message;
+            Status = "success";
+            StatusNo = 0;
+            MasterData = data;
+        } else {
+            Message = message;
+            Status = "failure";
+            StatusNo = 1;
+            MasterData = data;
+        }
     }
     response = {Message: Message, Status: Status, StatusNo: StatusNo, MasterData: MasterData};
     //console.log(response)
