d3e14381a246d2ed4711dfcf6b1910e5dbad134b
manishdixit18 <manishdixit18@gmail.com>
2018-01-29 14:43:26 +0530

    Clustering ,Fatal error handling and Logging with time stamp done.

--- bin/Logger.js
+++ bin/Logger.js
@@ -1,16 +1,20 @@
 var winston = require('winston');
-const { createLogger, transports } = winston;
-
+const { createLogger, format,transports } = winston;
+const { combine, timestamp, label, prettyPrint } = format;
 // Enable exception handling when you create your logger.
 const logger = winston.createLogger({
+   format: combine(
+    timestamp(),
+    prettyPrint()
+  ),
   transports: [
     new transports.File({ filename: './log/combined.log' ,timestamp:'true'}) 
   ],
   exceptionHandlers: [
     new transports.File({ filename: './log/exceptions.log',timestamp:'true' })
   ],
-  _uncaughtException :[
+  uncaughtException :[
        new transports.File({ filename: './log/error.log',timestamp:'true' })
-  ],timestamp: +new Date()
+  ]
 });
 module.exports=logger;
