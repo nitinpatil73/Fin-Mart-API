4adccda35a425da5a03615421c38c144e0b9471a
nitinpatil73 <nitinpatil2894@gmail.com>
2018-04-11 12:49:18 +0530

    get insimage

--- controller/SmartTermLifeController.js
+++ controller/SmartTermLifeController.js
@@ -183,7 +183,7 @@ var GetSmartTermLife = function(req, res, next) {
     			};
 
     			//quoteresponse.push({"termRequestEntity":response_quote});
-    			quoteresponse.push({"termRequestEntity":response_quote,"termRequestId": getsmartdata[0][i].lifetermrequestid,"statusProgress": 0,"insImage":" " });
+    			quoteresponse.push({"termRequestEntity":response_quote,"termRequestId": getsmartdata[0][i].lifetermrequestid,"statusProgress": 0,"insImage":getsmartdata[0][i].insImage });
     		}
 
     			for (var i = 0; i < getsmartdata[0].length; i++) {
@@ -242,7 +242,7 @@ var GetSmartTermLife = function(req, res, next) {
     			};
 
     			//applicationquote.push({"termRequestEntity":response_appli});
-    			applicationquote.push({"termRequestEntity":response_appli,"termRequestId": getsmartdata[1][i].lifetermrequestid,"statusProgress": 0,"insImage":""});
+    			applicationquote.push({"termRequestEntity":response_appli,"termRequestId": getsmartdata[1][i].lifetermrequestid,"statusProgress": 0,"insImage":getsmartdata[1][i].insImage});
     		}
 
     		var getsmart = {"quote":quoteresponse,"application":applicationquote};
