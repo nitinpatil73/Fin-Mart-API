50b3e5c311e10f9ddbe501dc5c68aa36ae4e3b22
nitinpatil73 <nitinpatil2894@gmail.com>
2018-04-20 12:19:03 +0530

    change uat.api server

--- controller/wrapper.js
+++ controller/wrapper.js
@@ -44,7 +44,12 @@ else if(hosttype == 2) {
     hostname = "finmartwebapi.magicsales.in";
   }
   else if(hosttype == 6) {
-    hostname = "api.rupeeboss.com";
+    if(process.env.NODE_ENV == 'development'){
+       hostname = "uat.api.rupeeboss.com";
+  }
+  else{
+       hostname = "api.rupeeboss.com";
+  }
   
   }else if(hosttype==7){
     hostname="http://vas.mobilogi.com";
@@ -88,6 +93,7 @@ else if(hosttype == 11) {
     method: method,
     headers: headers
   };
+  console.log("---------------------wrapper-----------------------------");
   console.log(options);
 
   var req = https.request(options, function(res) {
