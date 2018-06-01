c9487df09bfcb5808f268b239da041bb6a9e87de
daniyalpb <daniyal.shaikh@rupeeboss.com>
2018-06-01 16:38:56 +0530

    quick lead chagde

--- controller/QuickleadController.js
+++ controller/QuickleadController.js
@@ -63,6 +63,7 @@ wrapper('/BankAPIService.svc/createOtherLoanLeadReq', 'POST', {
 
 
 var quikleadfromrupeeboss = function (req, res, next) {
+  //dumpdatacontroller(req, res, next);
           var parameter = [];        
                 
           parameter.push(req.body.Broker_Id);
@@ -91,6 +92,57 @@ var quikleadfromrupeeboss = function (req, res, next) {
           });
 };
 
+var dumpdatacontroller = function(req, res, next) {
+    wrapper('/BankAPIService.svc/GetQuickLeadDetail', 'GET',{
+    },function(response) {
+      if(response != null && response != ''){
+        console.log(response.length);
+      console.log("---------------------------------------"); 
+        for(var i=0; i<response.length; i++){
+           console.log(i);
+          // var Lead_Date_Format = new Date(response[i].Lead_Date)
+          // var FollowUp_Date_Format = new Date(response[i].FollowUp_Date)
+          var dumpdataparameter = [];
+          dumpdataparameter.push(response[i].Broker_Id);
+          dumpdataparameter.push(response[i].Email);
+          dumpdataparameter.push(response[i].FBA_Id);
+        //  dumpdataparameter.push(formatDate(FollowUp_Date_Format));
+        //  dumpdataparameter.push(formatDate(Lead_Date_Format));
+
+          dumpdataparameter.push(response[i].FollowUp_Date);
+          dumpdataparameter.push(response[i].Lead_Date);
+
+          dumpdataparameter.push(response[i].Lead_Status_Id);
+          dumpdataparameter.push(response[i].Lead_id);
+          dumpdataparameter.push(response[i].Loan_Amt);
+          dumpdataparameter.push(response[i].Mobile);
+          dumpdataparameter.push(response[i].MonthlyIncome);
+          dumpdataparameter.push(response[i].Name);
+          dumpdataparameter.push(response[i].Product_Id);
+          dumpdataparameter.push(response[i].Remark);
+          //console.log(dumpdataparameter);
+          con.execute_proc('call quickleadrequest_data(?,?,?,?,?,?,?,?,?,?,?,?,?)',dumpdataparameter,function(savedata) {
+              if(savedata[0][0].SavedStatus == 0){
+                status=1;
+              }
+              else{
+                console.log(dumpdataparameter);
+                console.log(savedata);
+                status=0;
+              }
+          });
+        }
+        
+             base.send_response("Record saved successfully","Success",res);
+        //base.send_response("Success",response,res);
+      }else{
+        base.send_response("Failure",null,res);
+      }
+
+    },6);
+}
+
+
 // function formatDate(date) {
 //   var hours = date.getHours();
 //   var minutes = date.getMinutes();
@@ -102,4 +154,4 @@ var quikleadfromrupeeboss = function (req, res, next) {
 //   return date.getFullYear()+1 + "-" + date.getMonth() + "-" + date.getDate() + "  " + strTime;
 // }
 
-module.exports = {"QuickLead":QuickLead,"quikleadfromrupeeboss":quikleadfromrupeeboss};
+module.exports = {"QuickLead":QuickLead,"quikleadfromrupeeboss":quikleadfromrupeeboss,"dumpdatacontroller":dumpdatacontroller};
