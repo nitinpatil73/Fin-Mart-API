5d3ecdbc6382b61118899d4e08d2d1429a73f687
nitinpatil73 <nitinpatil2894@gmail.com>
2018-06-06 14:24:18 +0530

    changes

--- controller/smarthealth.js
+++ controller/smarthealth.js
@@ -203,8 +203,8 @@ var manageHealthRequest = function(req, res, next,responsedata,CustomerReference
 
 var parameter = [];
 
-if(req.body.HealthRequest.HealthRequestId){
-  parameter.push(req.body.HealthRequest.HealthRequestId); 
+if(req.body.HealthRequestId){
+  parameter.push(req.body.HealthRequestId); 
 }
 else{
   parameter.push(0);
@@ -228,7 +228,6 @@ parameter.push(req.body.HealthRequest.SumInsured);
 parameter.push(req.body.HealthRequest.SupportsAgentID);
 parameter.push(req.body.fba_id);
 parameter.push(req.body.agent_source);
-parameter.push(req.body.HealthRequest.Quote_Application_Status);
 
 //console.log("Length:"+req.body.HealthRequest.MemberList.length);
 var memberlist = "";
@@ -241,7 +240,7 @@ for (var i = 0; i < req.body.HealthRequest.MemberList.length; i++) {
  parameter.push(req.body.HealthRequest.pincode);
 //console.log(parameter);
 console.log("...............2435.............");
-  con.execute_proc('call ManageHealthRequest(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',parameter,function(data) {
+  con.execute_proc('call ManageHealthRequest(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',parameter,function(data) {
     console.log("............................");
     console.log(data[0][0]);
     if(data[0][0].SavedStatus=="0"){
